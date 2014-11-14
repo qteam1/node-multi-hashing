b444385de2e3adb7b20d00d222777cea76e9775e
ahmedbodi <ABodiwala@uclan.ac.uk>
2014-11-14 13:24:36 +0000

    Add JH/JHA algos

---
+++ jh.h
@@ -0,0 +1,16 @@
+#ifndef JH_H
+#define JH_H
+
+#ifdef __cplusplus
+extern "C" {
+#endif
+
+#include <stdint.h>
+
+void jh_hash(const char* input, char* output, uint32_t len);
+
+#ifdef __cplusplus
+}
+#endif
+
+#endif
