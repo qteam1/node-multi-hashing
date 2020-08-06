2b445a96f6e8f9e433d24a5a22e6151d2d524533
hackyminer <hackyminer@gmail.com>
2020-08-07 08:10:51 +0900

    add compat/mmap_win32.c

--- binding.gyp
+++ binding.gyp
@@ -75,6 +75,12 @@
             "cflags_cc": [
                 "-std=c++11"
             ],
+            "conditions": [
+                ['OS == "win"', {
+                    "sources": [ "compat/mmap_win32.c" ],
+                    "include_dirs": [ "compat" ]
+                }]
+            ]
         }
     ]
 }
---
+++ compat/mmap.h
@@ -0,0 +1,35 @@
+/** mmap() replacement for Windows
+ *
+ * Author: Mike Frysinger <vapier@gentoo.org>
+ * Placed into the public domain
+ */
+#ifndef COMPAT_MMAP_H
+#define COMPAT_MMAP_H
+
+#if defined(__MINGW32__) || defined(_WIN32)
+#include <sys/types.h>
+
+#define PROT_READ     0x1
+#define PROT_WRITE    0x2
+/* This flag is only available in WinXP+ */
+#ifdef FILE_MAP_EXECUTE
+#define PROT_EXEC     0x4
+#else
+#define PROT_EXEC        0x0
+#define FILE_MAP_EXECUTE 0
+#endif
+
+#define MAP_SHARED    0x01
+#define MAP_PRIVATE   0x02
+#define MAP_ANONYMOUS 0x20
+#define MAP_ANON      MAP_ANONYMOUS
+#define MAP_FAILED    ((void *) -1)
+
+void* mmap(void* start, size_t length, int prot, int flags, int fd, off_t offset);
+void munmap(void* addr, size_t length);
+
+#else
+#include <sys/mman.h>
+#endif
+
+#endif
