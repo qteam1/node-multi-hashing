db62cd5b6a85d171ec75ce2ae156d947df24e8b4
hackyminer <hackyminer@gmail.com>
2020-08-17 18:03:18 +0900

    reduce ethash.* remove prime numbers table.
     * remove dag_size, cache_size tables
     * ref: https://github.com/chfast/ethash/blob/master/lib/ethash/primes.c
     * clean up coding style
     * demystify some numbers

--- crypto/ethash.c
+++ crypto/ethash.c
@@ -7,9 +7,12 @@
 
 #define FNV_PRIME    0x01000193
 
+#define NODE_WORDS (64/4)
+#define MIX_WORDS (ETHASH_MIX_BYTES/4)
+#define MIX_NODES (MIX_WORDS / NODE_WORDS)
+
 #define fnv(x, y)    (((x) * FNV_PRIME) ^ (y))
 #define fnv_reduce(v)  fnv(fnv(fnv((v)[0], (v)[1]), (v)[2]), (v)[3])
-#define ETHASH_EPOCH_LENGTH 30000UL
 
 typedef struct _DAG128
 {
@@ -36,23 +39,23 @@ ethash_h256_t ethash_get_seedhash(uint64_t block_number)
 }
 
 // Output (cache_nodes) MUST have at least cache_size bytes
-static void EthGenerateCache(uint8_t *cache_nodes_in, const uint8_t *seedhash, uint64_t cache_size)
+static void ethash_generate_cache(uint8_t *cache_nodes_in, const uint8_t *seedhash, uint64_t cache_size)
 {
     uint32_t const num_nodes = (uint32_t)(cache_size / sizeof(node));
     node *cache_nodes = (node *)cache_nodes_in;
 
     SHA3_512(cache_nodes[0].bytes, seedhash, 32);
 
-    for(uint32_t i = 1; i < num_nodes; ++i) {
+    for (uint32_t i = 1; i < num_nodes; ++i) {
         SHA3_512(cache_nodes[i].bytes, cache_nodes[i - 1].bytes, 64);
     }
 
-    for(uint32_t j = 0; j < 3; j++) { // this one can be unrolled entirely, ETHASH_CACHE_ROUNDS is constant
-        for(uint32_t i = 0; i < num_nodes; i++) {
+    for (uint32_t j = 0; j < ETHASH_CACHE_ROUNDS; j++) { // this one can be unrolled entirely, ETHASH_CACHE_ROUNDS is constant
+        for (uint32_t i = 0; i < num_nodes; i++) {
             uint32_t const idx = cache_nodes[i].words[0] % num_nodes;
             node data;
             data = cache_nodes[(num_nodes - 1 + i) % num_nodes];
-            for(uint32_t w = 0; w < 16; ++w) { // this one can be unrolled entirely as well
+            for (uint32_t w = 0; w < NODE_WORDS; ++w) { // this one can be unrolled entirely as well
                 data.words[w] ^= cache_nodes[idx].words[w];
             }
 
@@ -61,27 +64,26 @@ static void EthGenerateCache(uint8_t *cache_nodes_in, const uint8_t *seedhash, u
     }
 }
 
-node CalcDAGItem(const node *CacheInputNodes, uint32_t NodeCount, uint32_t NodeIdx)
+node ethash_calc_dag_item(const node *cache_nodes, uint32_t num_nodes, uint32_t node_index)
 {
-    node DAGNode = CacheInputNodes[NodeIdx % NodeCount];
+    node dag_node = cache_nodes[node_index % num_nodes];
 
-    DAGNode.words[0] ^= NodeIdx;
+    dag_node.words[0] ^= node_index;
 
-    SHA3_512(DAGNode.bytes, DAGNode.bytes, sizeof(node));
+    SHA3_512(dag_node.bytes, dag_node.bytes, sizeof(node));
 
-    for(uint32_t i = 0; i < 256; ++i) {
-        uint32_t parent_index = fnv(NodeIdx ^ i, DAGNode.words[i % 16]) % NodeCount;
-        node const *parent = CacheInputNodes + parent_index; //&cache_nodes[parent_index];
+    for (uint32_t i = 0; i < ETHASH_DATASET_PARENTS; ++i) {
+        uint32_t parent_index = fnv(node_index ^ i, dag_node.words[i % NODE_WORDS]) % num_nodes;
+        node const *parent = &cache_nodes[parent_index];
 
-        for(int i = 0; i < 16; ++i) {
-            DAGNode.words[i] *= FNV_PRIME;
-            DAGNode.words[i] ^= parent->words[i];
+        for (uint32_t j = 0; j < NODE_WORDS; ++j) {
+            dag_node.words[j] = fnv(dag_node.words[j], parent->words[j]);
         }
     }
 
-    SHA3_512(DAGNode.bytes, DAGNode.bytes, sizeof(node));
+    SHA3_512(dag_node.bytes, dag_node.bytes, sizeof(node));
 
-    return DAGNode;
+    return dag_node;
 }
 
 static node *dag_cache = NULL;
@@ -90,18 +92,18 @@ static uint64_t current_epoch = -1;
 // output (result + mixhash) MUST have 64 bytes allocated (at least)
 void ethash_hash(const char* input, char* output, uint64_t height, uint64_t nonce)
 {
-    uint32_t MixState[32], TmpBuf[24];
+    uint32_t mixstate[32], tmpbuf[24];
     uint64_t epoch = height / ETHASH_EPOCH_LENGTH;
-    uint64_t DagSize;
-    uint32_t NodeCount = EthGetCacheSize(epoch) / sizeof(node);
+    uint64_t dagsize;
+    uint32_t num_nodes = ethash_get_cache_size(epoch) / sizeof(node);
 
     if (current_epoch != epoch) {
-        uint64_t cache_size = EthGetCacheSize(epoch);
+        uint64_t cache_size = num_nodes * sizeof(node);
         if (dag_cache)
             free(dag_cache);
         dag_cache = (node *)malloc(cache_size);
         ethash_h256_t seedhash = ethash_get_seedhash(height);
-        EthGenerateCache((uint8_t*)dag_cache, (uint8_t*)&seedhash, cache_size);
+        ethash_generate_cache((uint8_t*)dag_cache, (uint8_t*)&seedhash, cache_size);
         current_epoch = epoch;
     }
 
@@ -109,45 +111,44 @@ void ethash_hash(const char* input, char* output, uint64_t height, uint64_t nonc
     // run it through SHA3 - this becomes the initial value
     // for the mixing state buffer. The init value is used
     // later for the final hash, and is therefore saved.
-    memcpy(TmpBuf, input, 32);
-    memcpy(TmpBuf + 8, &nonce, 8);
-    //sha3_512((uint8_t *)TmpBuf, 64UL, (uint8_t *)TmpBuf, 40UL);
-    SHA3_512((uint8_t *)TmpBuf, (uint8_t *)TmpBuf, 40);
+    memcpy(tmpbuf, input, 32);
+    memcpy(tmpbuf + 8, &nonce, 8);
+    SHA3_512((uint8_t *)tmpbuf, (uint8_t *)tmpbuf, 40);
 
-    memcpy(MixState, TmpBuf, 64);
+    memcpy(mixstate, tmpbuf, 64);
 
     // The other half of the state is filled by simply
     // duplicating the first half of its initial value.
-    memcpy(MixState + 16, MixState, 64);
+    memcpy(mixstate + 16, mixstate, 64);
 
-    DagSize = EthGetDAGSize(epoch) / (sizeof(node) << 1);
+    dagsize = ethash_get_dag_size(epoch) / (sizeof(node) << 1);
 
     // Main mix of Ethash
-    for(uint32_t i = 0, Init0 = MixState[0], MixValue = MixState[0]; i < 64; ++i) {
-        uint32_t row = fnv(Init0 ^ i, MixValue) % DagSize;
-        node DAGSliceNodes[2];
-        DAGSliceNodes[0] = CalcDAGItem(dag_cache, NodeCount, row << 1);
-        DAGSliceNodes[1] = CalcDAGItem(dag_cache, NodeCount, (row << 1) + 1);
-        DAG128 *DAGSlice = (DAG128 *)DAGSliceNodes;
+    for (uint32_t i = 0, init0 = mixstate[0], mixvalue = mixstate[0]; i < ETHASH_ACCESSES; ++i) {
+        uint32_t row = fnv(init0 ^ i, mixvalue) % dagsize;
+        node dag_slice_nodes[2];
+        dag_slice_nodes[0] = ethash_calc_dag_item(dag_cache, num_nodes, row << 1);
+        dag_slice_nodes[1] = ethash_calc_dag_item(dag_cache, num_nodes, (row << 1) + 1);
+        DAG128 *dagslice = (DAG128 *)dag_slice_nodes;
 
-        for(uint32_t col = 0; col < 32; ++col) {
-            MixState[col] = fnv(MixState[col], DAGSlice->Columns[col]);
-            MixValue = col == ((i + 1) & 0x1F) ? MixState[col] : MixValue;
+        for (uint32_t col = 0; col < 32; ++col) {
+            mixstate[col] = fnv(mixstate[col], dagslice->Columns[col]);
+            mixvalue = col == ((i + 1) & 0x1F) ? mixstate[col] : mixvalue;
         }
     }
 
     // The reducing of the mix state directly into where
     // it will be hashed to produce the final hash. Note
     // that the initial hash is still in the first 64
-    // bytes of TmpBuf - we're appending the mix hash.
-    for(int i = 0; i < 8; ++i)
-        TmpBuf[i + 16] = fnv_reduce(MixState + (i << 2));
+    // bytes of tmpbuf - we're appending the mix hash.
+    for (int i = 0; i < 8; ++i)
+        tmpbuf[i + 16] = fnv_reduce(mixstate + (i << 2));
 
-    memcpy(output + 32, TmpBuf + 16, 32);
-    //memcpy(MixHash, TmpBuf + 16, 32);
+    memcpy(output + 32, tmpbuf + 16, 32);
+    //memcpy(mixhash, tmpbuf + 16, 32);
 
     // Hash the initial hash and the mix hash concatenated
     // to get the final proof-of-work hash that is our output.
     // final Keccak hash
-    SHA3_256((uint8_t*)output, (uint8_t *)TmpBuf, 64 + 32); // Keccak-256(s + compressed_mix)
+    SHA3_256((uint8_t*)output, (uint8_t *)tmpbuf, 64 + 32); // Keccak-256(s + compressed_mix)
 }
